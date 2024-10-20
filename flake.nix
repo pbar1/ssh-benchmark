fb3ae9bb9e118d02faa4208c5c7aadae28fcc337
Pierce Bartine <piercebartine@gmail.com>
2024-10-19 17:53:41 -0700

    flake

--- flake.nix
+++ flake.nix
@@ -8,26 +8,35 @@
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      naersk' = pkgs.callPackage naersk { };
+
+      server = naersk'.buildPackage {
+        pname = "server"; # Cargo workspace
+        src = ./.;
+      };
+      serverImage = pkgs.dockerTools.buildLayeredImage {
+        name = "ssh-server";
+        tag = "latest";
+        created = "now";
+        config = {
+          entrypoint = [ "${server}/bin/server" ];
+          Labels = {
+            "org.opencontainers.image.authors" = "pbar1";
+            "org.opencontainers.image.source" = "https://github.com/pbar1/ssh-benchmark";
+          };
         };
+      };
+    in
+    {
+      packages.aarch64-darwin = {
+        inherit server serverImage;
+      };
 
-        naersk' = pkgs.callPackage naersk { };
-
-      in
-      {
-        # Cargo workspace corresponds to `pname`
-
-        packages.server = naersk'.buildPackage {
-          pname = "server";
-          src = ./.;
-        };
-
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-      }
-    );
+      packages.x86_64-linux = {
+        inherit server serverImage;
+      };
+    };
 }
